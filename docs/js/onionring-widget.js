3af416f7556ba1eb952838113bfd9b80f20134ac
Lia <115422798+Liassica@users.noreply.github.com>
2023-04-07 13:37:49 -0500

    Simplified garfring by using spoofing
    Instead of trying to import a local copy of the master list, just spoof the URL to have the script think it's the neocities site
    
    This will even work on a local host

--- docs/js/onionring-widget.js
+++ docs/js/onionring-widget.js
@@ -7,22 +7,22 @@
 
 var tag = document.getElementById(ringID); //find the widget on the page
 
-thisSite = 'https://liassica.neocities.org/'; //Spoof the URL
+thisSite = "https://liassica.neocities.org/"; //Spoof the URL
 thisIndex = null;
 
 // go through the site list to see if this site is on it and find its position
-for (i = 0; i < localSites.length; i++) {
-  if (thisSite.startsWith(localSites[i])) { //we use startswith so this will match any subdirectory, users can put the widget on multiple pages
+for (i = 0; i < sites.length; i++) {
+  if (thisSite.startsWith(sites[i])) { //we use startswith so this will match any subdirectory, users can put the widget on multiple pages
     thisIndex = i;
     break; //when we've found the site, we don't need to search any more, so stop the loop
   }
 }
 
 function randomSite() {
-  otherlocalSites = localSites.slice(); //create a copy of the localSites list
-  otherlocalSites.splice(thisIndex, 1); //remove the current site so we don't just land on it again
-  randomIndex = Math.floor(Math.random() * otherlocalSites.length);
-  location.href = otherlocalSites[randomIndex];
+  otherSites = sites.slice(); //create a copy of the sites list
+  otherSites.splice(thisIndex, 1); //remove the current site so we don't just land on it again
+  randomIndex = Math.floor(Math.random() * otherSites.length);
+  location.href = otherSites[randomIndex];
 }
 
 //if we didn't find the site in the list, the widget displays a warning instead
@@ -36,11 +36,11 @@ if (thisIndex == null) {
   `);
 }
 else {
-  //find the 'next' and 'previous' localSites in the ring. this code looks complex
+  //find the 'next' and 'previous' sites in the ring. this code looks complex
   //because it's using a shorthand version of an if-else statement to make sure
-  //the first and last localSites in the ring join together correctly
-  previousIndex = (thisIndex - 1 < 0) ? localSites.length - 1 : thisIndex - 1;
-  nextIndex = (thisIndex + 1 >= localSites.length) ? 0 : thisIndex + 1;
+  //the first and last sites in the ring join together correctly
+  previousIndex = (thisIndex-1 < 0) ? sites.length-1 : thisIndex-1;
+  nextIndex = (thisIndex+1 >= sites.length) ? 0 : thisIndex+1;
 
   indexText = ""
   //if you've chosen to include an index, this builds the link to that
@@ -58,13 +58,13 @@ else {
   tag.insertAdjacentHTML('afterbegin', `
   <table>
     <tr>
-      <td class='webring-prev'><a href='${localSites[previousIndex]}'>previous</a></td>
+      <td class='webring-prev'><a href='${sites[previousIndex]}'>previous</a></td>
       <td class='webring-info'>This site is part of the ${ringName} webring</br>
       <span class='webring-links'>
         ${randomText}
         ${indexText}
         </span></td>
-      <td class='webring-next'><a href='${localSites[nextIndex]}'>next</a></td>
+      <td class='webring-next'><a href='${sites[nextIndex]}'>next</a></td>
     </tr>
   </table>
   `);
