ef35d0718da8a488e2a53e528b56dcfda3ce65ec
Liassica <115422798+Liassica@users.noreply.github.com>
2025-04-01 20:53:56 -0500

    feat: init flake with development shell

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    with pkgs;
+    {
+      devShells.${system}.default = mkShell {
+        nativeBuildInputs = [
+          http-server
+          nodePackages.prettier
+          soupault
+        ];
+      };
+    };
+}
