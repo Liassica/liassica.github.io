7100dc3b2288ce4520e4c913d6c592b22284d679
Liassica <115422798+Liassica@users.noreply.github.com>
2025-04-02 22:37:49 -0500

    feat: build 1

---
+++ docs/assets/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $("#safety-popup").after(function () {
+    return $(this).clone().removeAttr("id").attr("id", "safety-bottom");
+  });
+
+  $("#safety-bottom").append($("#safety-info").html());
+
+  $("#safety-popup h2")
+    .append('<i class="bi bi-chevron-up"></i>')
+    .wrapInner('<a href="#safety-bottom"></a>');
+});
+
+$(document).scroll(function () {
+  if (
+    $(this).scrollTop() + $(window).height() >
+    $("#safety-bottom").offset().top
+  ) {
+    $("#safety-popup").addClass("close");
+  } else {
+    $("#safety-popup").removeClass("close");
+  }
+});
